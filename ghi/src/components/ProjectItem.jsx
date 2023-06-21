baf60cf00084fd0a1b5cdc67d5c127a6764790f5
Chris <c.r.zambito@gmail.com>
2023-06-21 14:16:40 -0400

    update

--- ghi/src/components/ProjectItem.jsx
+++ ghi/src/components/ProjectItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { FaReact } from 'react-icons/fa';
 
-const ProjectItem = ({ img, title }) => {
+const ProjectItem = ({ img, title, tools, description, url }) => {
     return (
         <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
             <img src={img} alt="/" className="rounded-xl group-hover:opacity-10"/>
@@ -8,13 +9,43 @@ const ProjectItem = ({ img, title }) => {
                 <h3 className="text-2xl font-bold text-white tracking-wider text-center">
                     {title}
                 </h3>
-                <p className="pb-4 pt-2 text-white text-center">React JS</p>
-                <a href="">
-                    <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">More Info</p>
+                <p className="pb-2 pt-2 text-white text-center">{description}</p>
+                <p className="pb-4 text-white text-center">
+                    {tools.join(', ')}
+                </p>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                    <div className="flex items-center justify-center text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">
+                        <FaReact size={20} className="mr-2"/> Source Code
+                    </div>
                 </a>
             </div>
         </div>
     );
 };
 
-export default ProjectItem;
+export default ProjectItem
+
+
+
+
+
+// import React from "react";
+
+// const ProjectItem = ({ img, title }) => {
+//     return (
+//         <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+//             <img src={img} alt="/" className="rounded-xl group-hover:opacity-10"/>
+//             <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+//                 <h3 className="text-2xl font-bold text-white tracking-wider text-center">
+//                     {title}
+//                 </h3>
+//                 <p className="pb-4 pt-2 text-white text-center">React JS</p>
+//                 <a href="">
+//                     <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">More Info</p>
+//                 </a>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ProjectItem;
--- ghi/src/components/Projects.jsx
+++ ghi/src/components/Projects.jsx
@@ -5,21 +5,84 @@ import ithink from '../assets/ithink.png'
 import car from '../assets/carcar.png'
 import fjz from '../assets/fjz.png'
 
+const projectList = [
+    {
+        img: ithink,
+        title: 'iThink',
+        tools: ['React', 'MongoDB', 'FastAPI', 'Python', 'JavaScript', 'TailWindCSS'],
+        description: 'Using generative AI to help brainstorm ideas.',
+        url: 'https://gitlab.com/binary-bandits/i-think'
+    },
+    {
+        img: car,
+        title: 'CarCar',
+        tools: ['React', 'React Native', 'Django', 'Python', 'JavaScript', 'BootStrap'],
+        description: 'Your one stop shop for sales, inventory and service manager.',
+        url: 'https://gitlab.com/c.r.zambito/project-beta'
+    },
+    {
+        img: fjz,
+        title: 'FJZ LLC',
+        tools: ['React', 'TailWindCSS', 'Firebase', 'Vite'],
+        description: 'Tenant management services.',
+        url: 'https://gitlab.com/c.r.zambito/fjz'
+    },
+    {
+        img: propertyImg,
+        title: 'Studio 16 Task Manager',
+        tools: ['Vue.js', 'Firebase'],
+        description: 'This is a sample description for App4 project',
+        url: 'https://gitlab.com/team-163974311/studio-16-task-manager'
+    },
+    
+]
+
 const Projects = () => {
     return (
         <div id='projects' className='max-w[1040px] m-auto md:pl-20 p-4 py-16'>
             <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
             <p className='text-center py-8'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit neque id excepturi saepe veritatis minus deserunt ullam numquam, repellendus aliquid mollitia eligendi amet dolores sunt dolorem! Dicta possimus animi natus!
+                Personal Projects
             </p>
             <div className='grid sm:grid-cols-2 gap 12'>
-                <ProjectItem img={ithink} title='title of app1'/>
-                <ProjectItem img={car} title='title of app2'/>
-                <ProjectItem img={fjz} title='title of app3'/>
-                <ProjectItem img={propertyImg} title='title of app4'/>
+                {projectList.map(project => (
+                    <ProjectItem {...project} />
+                ))}
             </div>
         </div>
     )
 }
 
 export default Projects
+
+
+
+
+
+
+
+// import React from 'react'
+// import ProjectItem from "./ProjectItem"
+// import propertyImg from '../assets/butter.jpg'
+// import ithink from '../assets/ithink.png'
+// import car from '../assets/carcar.png'
+// import fjz from '../assets/fjz.png'
+
+// const Projects = () => {
+//     return (
+//         <div id='projects' className='max-w[1040px] m-auto md:pl-20 p-4 py-16'>
+//             <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
+//             <p className='text-center py-8'>
+//                 Personal Projects
+//             </p>
+//             <div className='grid sm:grid-cols-2 gap 12'>
+//                 <ProjectItem img={ithink} title='iThink'/>
+//                 <ProjectItem img={car} title='CarCar'/>
+//                 <ProjectItem img={fjz} title='FJZ LLC'/>
+//                 <ProjectItem img={propertyImg} title='title of app4'/>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Projects
